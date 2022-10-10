1f82d4e6dc2644489241b1b5318c03adc429cbda
feelfinn <kvachadzerrr@mail.ru>
2022-10-11 00:15:40 +0300

    feat: init firestore.service.ts

--- index.ts
+++ index.ts
@@ -1,10 +1,16 @@
 import * as express from "express";
-import * as path from 'path';
+import firestore from "./services/firestore.service";
+// Initialize Firebase
+
 
 const PORT = 3001
 const app = express()
-app.listen(PORT,()=>{
+
+
+app.listen(PORT,async () => {
+    await firestore.addData();
     console.log(`Server started on port ${PORT}`);
 })
 
-`    git config --global user.name "InvisibleAstra"`
+
+
